1685825a5737017715c4106bf3ccc20e2db6147b
Senthil Kumar <senthilktmz@outlook.com>
2025-09-25 20:57:09 +0530

    adding more chord patterns

--- pitchwiz/src/patterns/Chords.tsx
+++ pitchwiz/src/patterns/Chords.tsx
@@ -1,4 +1,16 @@
 
 export const CHORDS_PATTERNS_ARRAY = [
   ["Major", "1", "3", "5"],
+  ["Minor", "1", "b3", "5"],
+  ["sus2 / Suspended 2", "1", "2", "5"],
+  ["SUS / Suspended Fourth", "1", "4", "5"],
+  ["dim / DIMINISHED", "1", "b3", "b5"],
+  ["aug / AUGMENTED", "1", "3", "b6"],
+  //
+  //
+
+  ["♭5 / Flatted Fifth", "1","3", "b5"],
+  ["♭6 / Flatted Sixth", "1","3", "5", "b6"],
+  ["-♭6 / Minor Flatted Sixth", "1","b3", "5", "b6"],
+  ["6 / Major Sixth", "1", "3", "5", "6" ],
 ];
