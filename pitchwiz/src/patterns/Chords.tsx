8c01032616530a93aee3d921407ab854b7f176f8
Senthil Kumar <c_senthil.kumar@harness.io>
2025-09-27 10:05:03 +0530

    set 2 chords

--- pitchwiz/src/patterns/Chords.tsx
+++ pitchwiz/src/patterns/Chords.tsx
@@ -1,9 +1,8 @@
-
 export const CHORDS_PATTERNS_ARRAY = [
   ["Major", "1", "3", "5"],
   ["Minor", "1", "b3", "5"],
   ["sus2 / Suspended 2", "1", "2", "5"],
-  ["SUS / Suspended Fourth", "1", "4", "5"],
+  ["SUS / sus4 / Suspended Fourth", "1", "4", "5"],
   ["dim / DIMINISHED", "1", "b3", "b5"],
   ["aug / AUGMENTED", "1", "3", "b6"],
   //
@@ -13,4 +12,22 @@ export const CHORDS_PATTERNS_ARRAY = [
   ["♭6 / Flatted Sixth", "1","3", "5", "b6"],
   ["-♭6 / Minor Flatted Sixth", "1","b3", "5", "b6"],
   ["6 / Major Sixth", "1", "3", "5", "6" ],
+  ["-6 / Minor Sixth", "1", "b3", "5", "6"],
+  ["7 / Diminished Seventh", "1", "b3", "b5", "6"],
+  //
+  //
+  ["Q(3) / Quartal (Double Fourth)", "1", "4", "b7"],
+  ["7 / Dominant Seventh", "1", "3", "5", "b7"],
+  ["-7 / Minor Seventh", "1", "b3", "5", "b7"],
+  ["7sus2 / Dominant sus2", "1", "2", "5", "b7"],
+  ["7sus / Dominant Sus", "1", "4", "5", "b7"],
+  //
+  //
+  ["7b5 / Dominant Flat 5", "1", "3", "b5", "b7"],
+  ["ø / Half-Diminished", "1", "b3", "b5", "b7"],
+  ["7+ / Dominant Augmented", "1", "3", "b6", "b7"],
+  ["Δ / Delta (Major Seventh)", "1", "3", "5", "7"],
+  ["-Δ / Minor Delta (Minor Major Seventh)", "1", "b3", "5", "7"],
+  ["Δsus2 / Delta sus2", "1", "2", "5", "7"],
+
 ];
